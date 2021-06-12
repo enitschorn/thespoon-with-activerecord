9ee50b24fb9afcd8eb44a84e97aeab9e5789128e
Christine Schorn <cschorn91@gmail.com>
2021-06-12 12:24:26 +1000

    Refactor controller with before_action filter

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,11 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -19,18 +20,15 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurants_params)
 
     redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
 
     redirect_to restaurants_path
@@ -40,5 +38,9 @@ class RestaurantsController < ApplicationController
 
   def restaurants_params
     params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
